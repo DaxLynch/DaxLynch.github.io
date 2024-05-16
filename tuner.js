05c6d4f836721b0875dd917a7092312f7f84dee3
Dax Lynch <declanxlynch@gmail.com>
2024-05-16 16:04:57 -0700

    Working on the pitch classifier

--- tuner.js
+++ tuner.js
@@ -13,16 +13,39 @@ class Tuner {
 		source.connect(analyser);
 
 		// Set up the analyser
-		analyser.fftSize = 2048;
+        analyser.minDecibels = -80;
+		analyser.fftSize = 32768;
+        analyser.smoothingTimeConstant = 0;
+
 		const bufferLength = analyser.frequencyBinCount;
 		const dataArray = new Uint8Array(bufferLength);
+        
+        
+        function pitchClassAndCents(pitch){
+                if (pitch == 0) { return ["0", 0];};
+                let pitchInOctave0 = pitch / Math.pow(2, Math.trunc(Math.log2(pitch))-4)
+                let pitches = [27.5, 29.13523509488056, 30.867706328507698, 32.703195662574764, 34.647828872108946, 36.708095989675876, 38.890872965260044, 41.20344461410867, 43.65352892912541, 46.24930283895422, 48.99942949771858, 51.913087197493056]
+                let indexOfPitchAbove = pitches.findIndex((x) => {return x > pitchInOctave0}) //This is the pitch above
+                let centsBelowNextNote = 1200 * Math.log2(pitchInOctave0/pitches[indexOfPitchAbove]) 
+                let centsAbovePriorNote = 100 - centsBelowNextNote;
+                let pitchClasses = ["A","A#","B","C","C#","D","D#","E","F#","G","G#"]
+                if (centsAbovePriorNote <= -1 * centsBelowNextNote ){
+                    let noteIndex = indexOfPitchAbove - 1;
+                    return [pitchClasses[noteIndex], Math.round(centsAbovePriorNote)];
+                } else {
+                     let noteIndex = indexOfPitchAbove;
+                    return [pitchClasses[noteIndex], Math.round(centsBelowNextNote)];
+                }
+        };
+        
 
 		// Function to update the pitch display
 		const updatePitchDisplay = () => {
 		    analyser.getByteFrequencyData(dataArray);
 		    const maxIndex = dataArray.indexOf(Math.max(...dataArray));
-		    const pitch = Math.round(maxIndex * audioContext.sampleRate / analyser.fftSize);
-		    this.pitchDisplay.textContent = `${pitch} Hz`;
+		    let pitch = maxIndex * audioContext.sampleRate / analyser.fftSize;
+            let pCAC = pitchClassAndCents(pitch);
+		    this.pitchDisplay.textContent = `${pCAC[1]} cents away from ${pCAC[0]}`;
 		    requestAnimationFrame(updatePitchDisplay);
 		}
 
