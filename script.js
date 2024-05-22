93d90e7ee573e8f91a8be3df058bf523dfa2cc37
Dax Lynch <declanxlynch@gmail.com>
2024-05-22 12:21:45 -0700

    whoopsiey

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 const audioContext = new AudioContext();
 const tuner = new Tuner(audioContext, 'pitch-display');
 tuner.initialize();
-const metronome = new Metronome(audioContext)
+const metronome = new Metronome(audioContext);
+metronome.initialize();
 const rhythmTracker = new RhythmTracker(audioContext);
 rhythmTracker.initialize();
 
